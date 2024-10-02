3197d5a7d942b5a4e7f8eaab2b72b2f5823c0808
FedNirvanous451 <teodorzur@gmail.com>
2024-10-02 16:29:23 +0300

    to add next done progs

--- 3/vehicles/app/Main.java
+++ 3/vehicles/app/Main.java
@@ -1,12 +1,15 @@
-package vehicles;
+package vehicles.app;
+import vehicles.Car;
+import vehicles.ElectricCar;
+
 
 // реализация наследования и инкапсуляции
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Karla", "98134", "Diesel");
+        Car car1 = new Car("Шайтан", "98134", "Diesel");
         System.out.println(car1.get_ownerName());
-        System.out.println(car1.get_insuranceNumber());
+        System.out.println(car1.get_insuranceNumber()); 
         System.out.println(car1.get_engineType());
 
         ElectricCar electry = new ElectricCar("Max", "39580", 25.76);
@@ -15,5 +18,4 @@ public class Main {
         System.out.println(electry.get_insuranceNumber());
         System.out.println(electry.get_ownerName());
     }
-    
 }
